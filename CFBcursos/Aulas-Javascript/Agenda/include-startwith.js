23aaec66431e1d851ef470affe6dbc7cd329f492
Bruno Gerum <bruno.sedec@gmail.com>
2023-08-03 20:32:43 -0300

    Create include-startwith.js

---
+++ CFBcursos/Aulas-Javascript/Agenda/include-startwith.js
@@ -0,0 +1,11 @@
+//includes
+//endsWith
+//startsWith
+
+let nomes = ['Matheus','Lucas','Joao'];
+
+console.log(nomes.includes('Lucas'));
+
+let nome = 'Matheus';
+console.log(nome.endsWith('theus'));
+console.log(nome.startsWith('Mat'));
